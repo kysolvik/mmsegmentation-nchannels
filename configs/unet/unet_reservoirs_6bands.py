a175bc7dfa703f55efc9473c35a5111f5081a16e
Kylen Solvik <kysolvik@gmail.com>
2022-11-14 07:21:29 -0700

    Unet config for reservoirs

---
+++ configs/unet/unet_reservoirs_6bands.py
@@ -0,0 +1,26 @@
+_base_ = [
+    '../_base_/models/pspnet_unet_s5-d16.py',
+    '../_base_/datasets/reservoirs_6band.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+model = dict(
+    decode_head=dict(
+        num_classes=2,
+        out_channels=2,
+    loss_decode=dict(
+        type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0, class_weight=[1.0, 150.], #100 was pretty good
+         )),
+    auxiliary_head=dict(
+        num_classes=2,
+        out_channels=2,
+    loss_decode=dict(
+        type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0, class_weight=[1.0, 150.], # 100 seemed to work pretty well
+         ))
+    )
+evaluation = dict(
+    interval=1000,  # The interval of evaluation.
+    metric='mIoU')  # The evaluation metric.
+checkpoint_config = dict(  # Config to set the checkpoint hook, Refer to https://github.com/open-mmlab/mmcv/blob/master/mmcv/runner/hooks/checkpoint.py for implementation.
+    by_epoch=False,  # Whether count by epoch or not.
+    interval=1000)  # The save interval.
+

b47655770862fe13d9bfe787b7ff8aa53e1aad75
Kylen Solvik <kysolvik@gmail.com>
2022-12-20 18:43:47 -0700

    8band config with aug

--- configs/_base_/datasets/reservoirs_8band.py
+++ configs/_base_/datasets/reservoirs_8band.py
@@ -6,16 +6,16 @@ img_norm_cfg = dict(
           65.60963343, 141.6956218 ,  91.84874383, 180.89670207],
     std=[27.843059  , 35.68728997, 30.97197954, 44.67461069, 43.33962304,
          29.72645861, 21.93850306, 31.91190727], to_rgb=False)
-crop_size = (512, 512)
+crop_size = (256, 256)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_zero_label=False),
-#     dict(type='Resize', img_scale=(512, 512), ratio_range=(1.0, 1.5)),
-#     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='Resize', img_scale=(512, 512), ratio_range=(0.7, 1.5)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=1.0),
     dict(type='RandomFlip', prob=0.5),
 #     dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
-#     dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=1),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=1),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]

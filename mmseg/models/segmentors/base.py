390835b1f762e15b0498706904099075c6fa16c6
Kylen Solvik <kysolvik@gmail.com>
2023-01-03 09:28:19 -0700

    Pick first three bands for inference display

--- mmseg/models/segmentors/base.py
+++ mmseg/models/segmentors/base.py
@@ -274,6 +274,7 @@ class BaseSegmentor(BaseModule, metaclass=ABCMeta):
         # convert to BGR
         color_seg = color_seg[..., ::-1]
 
+        img = img[:,:,:3]
         img = img * (1 - opacity) + color_seg * opacity
         img = img.astype(np.uint8)
         # if out_file specified, do not show image in window

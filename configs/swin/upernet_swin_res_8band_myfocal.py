0525ea7b7d98b4751a0286c9c6ecf4c40673cfe4
Kylen Solvik <kysolvik@gmail.com>
2023-01-11 11:27:07 -0700

    Binary focal loss

---
+++ configs/swin/upernet_swin_res_8band_myfocal.py
@@ -0,0 +1,25 @@
+_base_ = [
+     '../_base_/models/upernet_swin.py',
+     '../_base_/datasets/reservoirs_8band.py',
+     '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k_lowlr.py'
+]
+model = dict(
+    decode_head=dict(
+        num_classes=2,
+        out_channels=1,
+    loss_decode=dict(
+        type='BinaryFocalLoss', loss_weight=2.0,
+         )),
+    auxiliary_head=dict(
+        num_classes=2,
+        out_channels=1,
+    loss_decode=dict(
+        type='BinaryFocalLoss', loss_weight=1.6,
+         ))
+    )
+evaluation = dict(
+    interval=1000,  # The interval of evaluation.
+    metric='mIoU')  # The evaluation metric.
+checkpoint_config = dict(  # Config to set the checkpoint hook, Refer to https://github.com/open-mmlab/mmcv/blob/master/mmcv/runner/hooks/checkpoint.py for implementation.
+    by_epoch=False,  # Whether count by epoch or not.
+    interval=1000)  # The save interval.
---
+++ mmseg/models/losses/binary_focal_loss.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from..builder import LOSSES
+from .utils import weighted_loss
+
+
+@weighted_loss
+def binary_focal_loss(pred, target, gamma=2.0, alpha=0.5, reduction='mean'):
+    assert pred.size() == target.size() and target.numel() > 0
+
+    logpt = F.sigmoid(pred)
+    logpt = logpt.gather(1, target)
+    logpt = logpt.view(-1)
+    pt = Variable(logpt.data.exp())
+
+    if alpha is not None:
+        at = alpha.gather(0, target.data.view(-1))
+        logpt = logpt * Variable(at)
+
+    loss = -1 * (1-pt)**gamma * logpt
+
+    if reduction == 'mean':
+        return loss.mean()
+    else:
+        return loss.sum()
+
+
+@LOSSES.register_module
+class BinaryFocalLoss(nn.Module):
+    def __init__(self, gamma=2, alpha=0.5, reduction='mean', loss_weight=1.0):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+        else:
+            self.alpha = torch.Tensor([alpha, 1-alpha])
+
+    def forward(self,
+                pred,
+                target,
+                weight=None,
+                avg_factor=None,
+                reduction_override=None):
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+        loss = self.loss_weight * binary_focal_loss(
+            pred, target, weight, reduction=reduction)
+        return loss
+
